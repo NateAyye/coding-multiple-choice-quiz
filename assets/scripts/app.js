4964b57a6460ea32465612fd534c29053245a4db
Nathan Cuevas <nathanacuevas97@gmail.com>
2023-04-10 19:11:15 -0700

    Added styles for Score Form, Started functionality for questions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,8 +12,46 @@ const scoreForm = document.getElementById('score-form');
 const finalScoreSpan = document.querySelector('#final-score');
 const initialsInput = document.getElementById('initials');
 const scoreFormButton = document.getElementById('score-submit-btn');
+const errorSpanEl = document.getElementById('error-span');
 
-const MAX_TIME = 5;
+const MAX_TIME = 30;
+const currentQuestions = getCurrentQuestions();
+
+let userScore = 0;
+let currentQuestion = 0;
+let initials = '';
+
+function getCurrentQuestions() {
+  let jumbledFixedQuestions = [];
+  let usedQuestions = [];
+  while (jumbledFixedQuestions.length < 10) {
+    const randomQuestion =
+      questions[Math.floor(Math.random() * questions.length)];
+    if (usedQuestions.includes(randomQuestion.id)) {
+      continue;
+    }
+    usedQuestions.push(randomQuestion.id);
+    jumbledFixedQuestions.push(randomQuestion);
+  }
+  return jumbledFixedQuestions;
+}
+
+function saveScore() {
+  const currentScores = JSON.parse(localStorage.getItem('highscores'));
+
+  if (initials.trim() === '') {
+    errorSpanEl.innerText = 'Initials must have a Value';
+    return;
+  }
+  const newScore = {
+    id: currentScores ? currentScores[currentScores.length - 1].id + 1 : 1,
+    initials,
+    score: userScore,
+  };
+
+  const newScores = currentScores ? [...currentScores, newScore] : [newScore];
+  localStorage.setItem('highscores', JSON.stringify(newScores));
+}
 
 // This function is going to hide the question-page and then display the score submit form
 function displayScoreForm() {
@@ -41,10 +79,19 @@ function startQuiz() {
   startSection.style.display = 'none';
   questionSection.style.display = 'flex';
   startTimer();
+  startGame();
+}
+
+function handleScoreSubmision(e) {
+  e.preventDefault();
 }
 
 function init() {
   startButton.addEventListener('click', startQuiz);
+  scoreFormButton.addEventListener('click', handleScoreSubmision);
+  initialsInput.addEventListener('input', (e) => {
+    initials = e.target.value;
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
